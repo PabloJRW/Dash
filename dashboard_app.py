a52a2f3e09af73800b320548c5f9bce93fcdb071
PabloJRW <ramoswilkinspj28@gmail.com>
2022-07-28 13:50:55 -0500

    plots ready

--- dashboard_app.py
+++ dashboard_app.py
@@ -11,6 +11,7 @@ from turtle import color, title
 # DATA
 # ==============================================================================
 yt_top = pd.read_csv('top_200_youtubers.csv')
+yt_top = yt_top.drop_duplicates(subset='Channel Name', keep='last')
 
 # Dash App
 # ==============================================================================
@@ -22,9 +23,9 @@ app.layout = html.Div(children=[
     # Header -----------------------------------------------------------
     html.Div(children=[
         html.H1("YouTube", 
-            style={'font-style':'Bold','color':'#282828', 'margin':'0px', 'padding':'1.5% 0% 0% 1.5%'}),
+            style={'font-size':'42px','font-style':'Youtube Sans Bold','color':'black','margin':'0px', 'padding':'1.5% 0% 0% 1.5%'}),
         html.H2("Top 200 Youtubers", 
-            style={'color':'black', 'font-size':'81%','margin':'0%', 'padding':'0% 0% 1% 1.5%'}),
+            style={'color':'white', 'font-size':'81%','margin':'0%', 'padding':'0% 0% 1% 1.5%'}),
     ], style={'width':'100vw', 'height':'9%','margin':'0% 0% 0.6% 0%','border':'.5px solid gray','background':'#FF0000','border-radius':'12px'}),
 
 
@@ -32,8 +33,8 @@ app.layout = html.Div(children=[
         # Dropdown - Selección de región -----------------------------------
         html.Div(children=[
             html.Div(children=[
-                html.H2("Country", style={'margin':'0% 0% 0% 6%', 'padding':'0%', 'color':'black'}),
-                dcc.Dropdown(id='region_dd',
+                html.H2("Country", style={'margin':'0% 0% 0% 6%', 'padding':'0%', 'color':'white'}),
+                dcc.Dropdown(id='country_dd',
                     options=[
                         {'label':'Todas ', 'value':False},
                         {'label':'Norte','value':'North'},
@@ -56,12 +57,12 @@ app.layout = html.Div(children=[
         html.Div(children=[
             dcc.Graph(id='mvcategory')
             ],style={'width':'620px','height':'320px','background':'white','padding':'1%','border':'1px solid gray',
-            'border-radius':'12px','margin':'6px 24px 24px 24px'}),
+            'border-radius':'12px','margin':'6px 12px 24px 24px'}),
 
         # Gráfico de ventas por región
         html.Div(children=[
             dcc.Graph(id='maintopics')
-            ], style={'width':'620px','height':'320px','background':'white','padding':'1%','margin':'6px 24px 0px 24px','border':'1px solid gray',
+            ], style={'width':'620px','height':'320px','background':'white','padding':'1%','margin':'6px 24px 0px 12px','border':'1px solid gray',
                 'border-radius':'12px'})
 
     # 1ra fila - cierre del div de los gráficos
@@ -75,11 +76,15 @@ app.layout = html.Div(children=[
         # Line Plot: Order Date
         html.Div(children=[
             dcc.Graph(id="topfollowers")
-        ], style={'width':'620px','height':'320px','background':'white','padding':'0','margin':'6px 24px 0px 24px','border':'1px solid gray',
-            'border-radius':'12px'})
+        ], style={'width':'620px','height':'320px','background':'white','padding':'1%','margin':'6px 12px 0px 24px','border':'1px solid gray',
+            'border-radius':'12px'}),
 
         # Plot: 
-
+        # Line Plot: Order Date
+        html.Div(children=[
+            dcc.Graph(id="likes")
+        ], style={'width':'620px','height':'320px','background':'white','padding':'1%','margin':'6px 24px 0px 12px','border':'1px solid gray',
+            'border-radius':'12px'})
 
     # 2da fila: cierre del div
     ], style={'display':'flex'}),
@@ -92,7 +97,8 @@ app.layout = html.Div(children=[
     Output(component_id='mvcategory', component_property='figure'),
     Output(component_id='maintopics', component_property='figure'),
     Output(component_id='topfollowers', component_property='figure'),
-    Input(component_id='region_dd', component_property='value')
+    Output(component_id='likes', component_property='figure'),
+    Input(component_id='country_dd', component_property='value')
 )
 def updatePlots(region):
     # Ensure the DataFrame is not overwritten
@@ -102,23 +108,31 @@ def updatePlots(region):
         data = data[data['Region']==region]
     
      # plot - ventas por categorias
-    mv_category = yt_top.value_counts('Main Video Category')
+    mv_category = yt_top.value_counts('Main Video Category').head(5)
     mv_category_fig = px.bar(mv_category, title="Top Categories", width=600, height=300)
     mv_category_fig.update_traces(marker_color='#FF0000')
-    mv_category_fig.update_layout(xaxis_title=None,yaxis_title='Ventas',showlegend=False)
+    mv_category_fig.update_layout(xaxis_title=None,yaxis_title='Number of Channels',showlegend=False)
     
     # plot - ventas por subcategorias
-    main_topics = yt_top.value_counts('Main topic')
+    main_topics = yt_top.value_counts('Main topic').head(5)
     main_topics_fig = px.bar(main_topics, title="Top Topics", width=600, height=300)
     main_topics_fig.update_traces(marker_color='#FF0000')
-    main_topics_fig.update_layout(xaxis_title=None,yaxis_title='Ventas',showlegend=False)
+    main_topics_fig.update_layout(xaxis_title=None,yaxis_title='number of Channels',showlegend=False)
     
     #lineplot - order date
-    top_followers_fig = px.bar(yt_top, x="Channel Name", y="followers")
+    top_followers = yt_top[['Channel Name','followers']].sort_values('followers', ascending=False).head()
+    top_followers_fig = px.bar(top_followers, x="Channel Name", y="followers", title="Top Followers",
+        text_auto='.2s', width=600, height=300)
     top_followers_fig.update_traces(marker_color='#FF0000')
-    top_followers_fig.update_layout(xaxis_title=None,yaxis_title='Ventas',showlegend=False)
+    top_followers_fig.update_layout(xaxis_title=None,yaxis_title='Number of Followers',showlegend=False)
     
-    return mv_category_fig, main_topics_fig, top_followers_fig
+    #
+    top_likes = yt_top[['Channel Name','Likes']].sort_values('Likes', ascending=False).head(5)
+    top_likes_fig = px.bar(top_likes, x="Channel Name", y="Likes", width=600, height=300)
+    top_likes_fig.update_traces(marker_color='#FF0000')
+    top_likes_fig.update_layout(xaxis_title=None,yaxis_title='Number of Likes',showlegend=False)
+    
+    return mv_category_fig, main_topics_fig, top_followers_fig, top_likes_fig
 
 
 if __name__ == '__main__':
